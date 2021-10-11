528bfe448e9ad6540abf7daad43caa75381ca6f3
Qais Yousuf <qais@seosoft.dk>
2021-10-11 13:15:27 +0200

    Creating Base ViewModel

---
+++ OnlineSurvey.ViewModel/BaseViewModel.cs
@@ -0,0 +1,55 @@
+﻿using OnlineSurvey.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineSurvey.ViewModel
+{
+    public class BaseViewModel
+    {
+        public BaseViewModel()
+        {
+            FooterLinks = new List<FooterLinks>();
+        }
+        public int Id { get; set; }
+
+       
+        [Display(Name = "Site title")]
+        public string SiteTitle { get; set; }
+
+       
+        [Display(Name = "Site Name")]
+        public string SiteName { get; set; }
+
+       
+        [Display(Name = "Site Owner")]
+        public string SiteOwner { get; set; }
+        public DateTime SiteLastUpdatedDate { get; set; }
+
+        
+        [Display(Name = "Site Content")]
+        public string SiteContent { get; set; }
+
+        [Required]
+        [Display(Name = "Designed by")]
+        public string DesignedBy { get; set; }
+
+        
+        [Display(Name = "Site copyright")]
+        public string Sitecopyright { get; set; }
+
+        
+        [Display(Name = "Animation url")]
+        public string AnimationUrl { get; set; }
+
+        [Display(Name = "Is Active Site Footer")]
+        public bool IsSiteFooterActive { get; set; }
+
+        [Display(Name = "Footer Links")]
+        public int[] FooterLinksId { get; set; }
+
+        public List<string> FooterLinksTag { get; set; }
+
+        public List<FooterLinks> FooterLinks { get; set; }
+    }
+}
