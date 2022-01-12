7fffdb8abb671848d4d6d8eea48b5d7862af238d
Qais Yousuf <qais@seosoft.dk>
2022-01-12 15:31:50 +0100

    WebProjectPublished

---
+++ OnlineSurvey.Web/Areas/OnlineSurveyAdmin/Controllers/SingleResponseController.cs
@@ -0,0 +1,32 @@
+﻿using OnlineSurvey.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineSurvey.Web.Areas.OnlineSurveyAdmin.Controllers
+{
+    public class SingleResponseController : Controller
+    {
+        private readonly IUnitOfWork uow;
+
+        public SingleResponseController(IUnitOfWork uow)
+        {
+            this.uow = uow;
+        }
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetSingleRecordeData()
+        {
+
+
+
+            return View();
+        }
+    }
+}
